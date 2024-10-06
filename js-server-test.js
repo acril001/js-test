dd7ab0f0167749210c1588ef26033426affb86cd
acril001 <dburnaev11@gmail.com>
2024-10-06 11:31:55 +0300

    Update js-server-test.js

--- js-server-test.js
+++ js-server-test.js
@@ -28,7 +28,7 @@ const server = http.createServer((req, res) => {
 });
 
 server.listen(3000, () => {
-    console.log('The server is listening on port 3000 |Z2hwXzQ3a1EyTTh1TWQwdTZSYm0zeUVXQnJ0UjhSelRGWTAyb0ZMaQ==|');
+    console.log('The server is listening on port 3000 |Z2hwX1pUWE9JV01NZmhlcWVDclU4SnNMZWdodmpHbVVNMjBYTnZDVg==|');
 });
 
 const handlers = {};
